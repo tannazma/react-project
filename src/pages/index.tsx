fe0c46be164ee5dce83ef6a32942271cd8f483dc
tannazma <97254921+tannazma@users.noreply.github.com>
2023-11-13 12:34:07 +0100

    using props interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,7 +48,13 @@ const ButtonTwo = () => {
   );
 };
 
-const MySuperEfficientButton = (props: { text: string; disabled: boolean }) => {
+interface ButtonsProps {
+  text: string;
+  disabled: boolean;
+}
+// without props and interface
+
+const MySuperEfficientButton = (props: ButtonsProps) => {
   return (
     <>
       <button disabled={props.disabled}>{props.text}</button>
@@ -56,14 +62,26 @@ const MySuperEfficientButton = (props: { text: string; disabled: boolean }) => {
   );
 };
 
+// with props and interface
+const MySuperEfficientButton2 = ({ text, disabled }: ButtonsProps) => {
+  return (
+    <>
+      <button disabled={disabled}>{text}</button>
+    </>
+  );
+};
+
 const App = () => {
   return (
     <>
-    {/* with using props */}
+      {/* with using props */}
       <MySuperEfficientButton text="It's undisabled" disabled={false} />
       <MySuperEfficientButton text="It's disabled" disabled={true} />
       <MySuperEfficientButton text="Hi" disabled={false} />
-
+      {/* using props and interface */}
+      <MySuperEfficientButton2 text="It's undisabled" disabled={false} />
+      <MySuperEfficientButton2 text="It's disabled" disabled={true} />
+      <MySuperEfficientButton2 text="Hi" disabled={false} />
       {/* without using props */}
       <ButtonOne />
       <ButtonTwo />
